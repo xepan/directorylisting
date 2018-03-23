51b3ae6e3274df78dd3aa555cded1a8aca1f6c2a
sinharakesh <rksinha.btech@gmail.com>
2018-03-23 12:54:08 +0530

    list fields added

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -15,7 +15,7 @@ class Initiator extends \Controller_Addon {
 		$this->app->listing_menu = $this->app->top_menu->addMenu('Listing');
 		// $this->app->listing_menu->addItem(['Contact','icon'=>' fa fa-sitemap'],'xepan_listing_contact');
 		$this->app->listing_menu->addItem(['Listing List','icon'=>' fa fa-file-text-o'],'xepan_listing_list');
-		$this->app->listing_menu->addItem(['Listing Fields','icon'=>' fa fa-file-text-o'],'xepan_listing_field');
+		// $this->app->listing_menu->addItem(['Listing Fields','icon'=>' fa fa-file-text-o'],'xepan_listing_field');
 		
 		return $this;
 
