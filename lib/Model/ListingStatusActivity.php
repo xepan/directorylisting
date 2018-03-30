a2c992b24f8c80587dc96b45592e0dac401ffc3a
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-03-30 11:25:42 +0530

    used company config model

--- lib/Model/ListingStatusActivity.php
+++ lib/Model/ListingStatusActivity.php
@@ -29,25 +29,7 @@ class Model_ListingStatusActivity extends \xepan\base\Model_Table{
 		$this->addField('sms_send_to_list_data_fields')->display(array('form'=>'xepan\base\NoValidateDropDown'));
 		$this->addField('sms_send_to_custom_phone_numbers')->type('text');
 
-		$company_m = $this->add('xepan\base\Model_ConfigJsonModel',
-				[
-					'fields'=>[
-								'company_name'=>"Line",
-								'company_owner'=>"Line",
-								'mobile_no'=>"Line",
-								'company_email'=>"Line",
-								'company_address'=>"Line",
-								'company_pin_code'=>"Line",
-								'company_description'=>"xepan\base\RichText",
-								'company_logo_absolute_url'=>"Line",
-								'company_twitter_url'=>"Line",
-								'company_facebook_url'=>"Line",
-								'company_google_url'=>"Line",
-								'company_linkedin_url'=>"Line",
-								],
-					'config_key'=>'COMPANY_AND_OWNER_INFORMATION',
-					'application'=>'communication'
-				]);
+		$company_m = $this->add('xepan\base\Model_Config_CompanyInfo');
 			$company_m->tryLoadAny();
 
 		$this->getElement('email_send_to_custom_email_ids')->defaultValue($company_m['company_email']);
