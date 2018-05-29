2de447f70fed3710ca43327dbd5ea121d1f2a0fe
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-05-29 15:21:22 +0530

    update app page implemented

---
+++ page/updateapp.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace xepan\listing;
+
+class page_updateapp extends \xepan\base\page_updateapp {
+	public $application_namespace= 'xepan\listing';
+	public $source_uri='https://github.com/xepan/listing/archive/master.zip';
+	public $sub_path='listing-master';
+}
